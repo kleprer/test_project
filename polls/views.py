60bda2d8c50278685e4e87243099fad416c660e9
Maksimova Snezhana <steph.klepa@gmail.com>
2026-05-04 01:23:25 +0300

    testing urls setup in project

--- polls/views.py
+++ polls/views.py
@@ -1,2 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def poll(request):
+    return HttpResponse("Hello, poll. .")
+
+def index(request):
+    return HttpResponse("Hello, index .")
 
