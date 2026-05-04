5b725aa0931bf7225b2ea6a0c1d3a169cdb0577c
Maksimova Snezhana <steph.klepa@gmail.com>
2026-05-05 01:49:21 +0300

    added vote and results views + templates

--- polls/views.py
+++ polls/views.py
@@ -1,9 +1,9 @@
-from unittest import loader
-
+from django.db.models import F
 from django.shortcuts import get_object_or_404, render
-from django.http import Http404, HttpResponse
+from django.http import Http404, HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
-from polls.models import Question
+from polls.models import Choice, Question
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
@@ -21,3 +21,25 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes = F("votes") + 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+    
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
