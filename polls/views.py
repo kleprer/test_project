f39025a0d9c72c4e2877e274e41238d2a0ee552e
Maksimova Snezhana <steph.klepa@gmail.com>
2026-05-09 02:54:58 +0300

    removed dublicate vote func

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,6 @@
 from django.db.models import F
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.utils import timezone
 from django.views import generic 
@@ -30,10 +30,8 @@ class ResultsView(generic.DetailView):
     template_name = "polls/results.html"
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
-
-def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
+    
     try:
         selected_choice = question.choice_set.get(pk=request.POST["choice"])
     except (KeyError, Choice.DoesNotExist):
